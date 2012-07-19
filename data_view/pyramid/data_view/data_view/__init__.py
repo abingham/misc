1fbb6c072e3d2e3f8a5047d726d3989d00b2b6df
Austin Bingham <austin.bingham@roxar.com>
2012-07-19 15:37:05 +0200

    tweaking URL routes.

--- data_view/pyramid/data_view/data_view/__init__.py
+++ data_view/pyramid/data_view/data_view/__init__.py
@@ -7,7 +7,7 @@ def main(global_config, **settings):
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('home', '/')
     config.add_route('cube_info', '/info')
-    config.add_route('view_slice', '/slice/{axis}/{index}')
+    config.add_route('view_slice', '/slice/{axis}*index')
     config.add_route('slice_image', '/image/{axis}/{index}')
     config.scan()
     return config.make_wsgi_app()
